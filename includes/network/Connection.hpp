904788ed5aff66c23734d620fa76605a092bcd0d
Kauana Tombolato <kauanatomb@gmail.com>
2026-02-16 16:48:07 +0100

    added connection class idea with functions helpers

---
+++ includes/network/Connection.hpp
@@ -0,0 +1,36 @@
+#include "../resolver/RuntimeConfig.hpp"
+
+class Connection {
+    public:
+        Connection(int fd, const RuntimeConfig& cfg);
+
+        void onReadable();
+        void onWritable();
+
+        bool isClosed() const;
+        bool wantsWrite() const;
+
+        int getFd() const;
+
+    private:
+        int _socket_fd;
+        std::string _read_buffer;
+        std::string _write_buffer;
+
+        RequestParser _parser;
+        HttpRequest _request;
+        HttpResponse _response;
+
+        const RuntimeConfig& _config;
+
+        enum class ConnectionState { 
+            READING, // waiting for client data 
+            PARSING, // parse in process 
+            HANDLING, // RequestHandler active 
+            WRITING, // sending response 
+            CLOSED // connection ready to be closed 
+        };
+
+        ConnectionState _state;
+        bool _keep_alive;
+};
